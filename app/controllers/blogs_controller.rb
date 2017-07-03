7a33e3835bcb33e3164769e0674de41605aa1da1
Martin Velek <martin.velek@hotmail.com>
2017-07-03 23:14:01 +0200

    Adjust blogs_controller.rb to conform to rubocop coding standards

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  # before_action :set_blog, only: %i[show edit update destroy]
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   # GET /blogs
@@ -9,8 +10,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs/1
   # GET /blogs/1.json
-  def show
-  end
+  def show; end
 
   # GET /blogs/new
   def new
@@ -18,17 +18,18 @@ class BlogsController < ApplicationController
   end
 
   # GET /blogs/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /blogs
   # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
-
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.html do
+          redirect_to @blog,
+                      notice: 'Blog was successfully created.'
+        end
       else
         format.html { render :new }
       end
@@ -40,30 +41,38 @@ class BlogsController < ApplicationController
   def update
     respond_to do |format|
       if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+        format.html do
+          redirect_to @blog,
+                      notice: 'Blog was successfully updated.'
+        end
       else
         format.html { render :edit }
       end
     end
   end
 
-  # DELETE /blogs/1
+  # DELETE /blogs/1jjj
   # DELETE /blogs/1.json
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully deleted.' }
+      format.html do
+        redirect_to blogs_url,
+                    notice: 'Blog was successfully deleted.'
+      end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def blog_params
-      params.require(:blog).permit(:title, :body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list
+  # through.
+  def blog_params
+    params.require(:blog).permit(:title, :body)
+  end
 end
